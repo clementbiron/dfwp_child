e94fc7d63d99ccbec3c281ce318acc6d6534d193
Clément Biron <clement.biron@gmail.com>
2015-04-28 11:53:03 +0200

    message d'erreur compatibilité IE gérer maintenant sans php évite des configs de cache pour des users agents spéficiques

--- footer.php
+++ footer.php
@@ -2,6 +2,15 @@
 			//For plugins and other
 			wp_footer();
 		?>
+
+		<div class="dfwp_warningIE">
+	  		<div class="dfwp_warningIE_bg"></div>
+			<div class="dfwp_warningIE_msg">
+				<h2>Votre navigateur est obsolète</h2>
+				<p>Vous risquez de ne pas pouvoir profiter pleinement de la navigation sur ce site.</p>
+				<p>Procédez à une mise à jour de votre <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx"><u>nagivateur</u></a> ou <a href="https://www.mozilla.org/fr/firefox/new/"><u>changez en</u></a>.</p>
+			</div>
+		</div>
    
 		<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/deploy/js/script.js"></script>
 		<script type="text/javascript">
--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7 ie6"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie10 lt-ie9 lt-ie8 ie7"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9 ie8"> <![endif]-->
-<!--[if IE 9]>         <html class="no-js lt-ie10 lt-ie9 ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10 ie9"> <![endif]-->
 <?php if (stripos($_SERVER['HTTP_USER_AGENT'], 'MSIE 10')): ?>
-	<html class="no-js lt-ie10 ie10">
+	<html class="no-js ie10">
 <?php else: ?>
 	<html class="no-js">
 <?php endif; ?>
@@ -26,17 +26,3 @@
 	 ?>
 </head>
 <body class="<?php echo join(' ', get_body_class()); ?>">
-
-	<?php 
-		//IE 6, 7, 8, 9 message
-		if((strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== FALSE) ||  
-			(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 7.') !== FALSE) || 
-			(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 8.') !== FALSE) || 
-			(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 9.') !== FALSE)){
-			echo '<div class="dfwp_warningIE">';
-				echo '<h2>Votre navigateur est obsolète</h2>';
-				echo '<p>Vous risquez de ne pas pouvoir profiter pleinement de la navigation sur ce site.</p>';
-				echo '<p>Procédez à une mise à jour de votre <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx"><u>nagivateur</u></a> ou <a href="https://www.mozilla.org/fr/firefox/new/"><u>changez en</u></a>.</p>';
-			echo '</div>';
-		}
-	?>
