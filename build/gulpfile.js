f97c97964c0d5ec2891c41eba447271312b10a94
Clément Biron <clement.biron@gmail.com>
2016-11-23 08:37:27 +0100

    styledown ready

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -3,107 +3,117 @@
 /**
  * Charger les dépendances
  */
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var csso = require('gulp-csso');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var csso         = require('gulp-csso');
 var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var livereload = require('gulp-livereload');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var spritesmith = require('gulp.spritesmith');
-var rename = require('gulp-rename');
-var styledown = require('gulp-styledown');
+var concat       = require('gulp-concat');
+var uglify       = require('gulp-uglify');
+var livereload   = require('gulp-livereload');
+var plumber      = require('gulp-plumber');
+var notify       = require('gulp-notify');
+var spritesmith  = require('gulp.spritesmith');
+var rename       = require('gulp-rename');
+var styledown    = require('gulp-styledown');
 
 //Config des erreurs
 var notifyError = {
-	title: "Error",
+	title  : "Error",
 	message: "<%= error.message %>"
 }
 
-//Sprites
+/**
+ * Sprites
+ */
 gulp.task('sprites', function ()
 {
 	var sprites = [
 		{
-			name : "1x"
+			name: "1x"
 		},
 		{
-			name : "2x"
+			name: "2x"
 		}
 	];
 
-	sprites.forEach(function(el)
+	sprites.forEach(function (el)
 	{
-		var spriteData = gulp.src('../src/sprites/'+el.name+'/*.png')
+		var spriteData = gulp.src('../src/sprites/' + el.name + '/*.png')
 			.pipe(plumber({
-				errorHandler:notify.onError(notifyError)
+				errorHandler: notify.onError(notifyError)
 			}))
 			.pipe(spritesmith({
-				imgName: 'sprite'+el.name+'.png',
-				cssName: 'sprite'+el.name+'.scss',
-				cssTemplate: 'sprites-templates/sprite'+el.name+'.scss.handlebars'
+				imgName    : 'sprite' + el.name + '.png',
+				cssName    : 'sprite' + el.name + '.scss',
+				cssTemplate: 'sprites-templates/sprite' + el.name + '.scss.handlebars'
 			}));
-		var imgStream = spriteData.img.pipe(gulp.dest('../dist/img/'+el.name+'/'));
-		var cssStream = spriteData.css.pipe(gulp.dest('../src/sprites/'+el.name+'/'));
+		var imgStream  = spriteData.img.pipe(gulp.dest('../dist/img/' + el.name + '/'));
+		var cssStream  = spriteData.css.pipe(gulp.dest('../src/sprites/' + el.name + '/'));
 	});
 });
 
 /**
  * CSS
  */
-gulp.task('styles',function()
+gulp.task('styles', function ()
 {
 	//Tableaux des points d'entrées des css à générer
 	//Ici on génère bien 3 feuille de styles différentes
 	var stylesBootstrap = [
 		{
-			name : "index",
+			name: "index",
 			src : '../src/bootstrap/index.scss'
 		},
 		{
-			name : "pattern",
+			name: "pattern",
 			src : '../src/common/layout/pattern.scss'
 		},
 		{
-			name : "maintenance",
+			name: "maintenance",
 			src : '../src/common/layout/maintenance.scss'
 		}
 	];
 
 	//Pour chaque styles
-	stylesBootstrap.forEach(function(el)
+	stylesBootstrap.forEach(function (el)
 	{
 		gulp.src(el.src)
 			.pipe(plumber({
-				errorHandler:notify.onError(notifyError)
+				errorHandler: notify.onError(notifyError)
 			}))
 			.pipe(sass().on('error', sass.logError))
 			.pipe(autoprefixer("> 4%"))
-			.pipe(concat(el.name +'.css'))
+			.pipe(concat(el.name + '.css'))
 			.pipe(gulp.dest('../dist/css/'))
 
 			//Pour la version minifié
 			.pipe(csso())
-			.pipe(concat(el.name +'.min.css'))
+			.pipe(concat(el.name + '.min.css'))
 			.pipe(gulp.dest('../dist/css/'))
 			.pipe(livereload());
 	});
 });
 
-gulp.task('styleguide', function()
+/**
+ * Styleguide
+ */
+gulp.task('styleguide', function ()
 {
-	gulp.src('../src/components/**/*.scss')
+	gulp.src([
+			'../src/bootstrap/*.scss',
+			'../src/common/**.scss',
+			'../src/common/**/*.scss',
+			'../src/components/**/*.scss'
+		])
 		.pipe(styledown({
-			config: '../styleguide/config.md',
+			config  : '../styleguide/config.md',
 			filename: 'styleguide.html'
 		}))
 		.pipe(gulp.dest('../styleguide'));
 });
 
 /**
- * Tâche js
+ * JS
  */
 gulp.task('scripts', function ()
 {
@@ -115,21 +125,21 @@ gulp.task('scripts', function ()
 			'../src/bootstrap/bootstrap.js' //Project bootstrap
 		])
 		.pipe(plumber({
-			errorHandler:notify.onError(notifyError)
+			errorHandler: notify.onError(notifyError)
 		}))
 		.pipe(concat('index.js'))
 		.pipe(gulp.dest('../dist/js/'))
-		.pipe(rename({ extname: '.min.js' }))
+		.pipe(rename({extname: '.min.js'}))
 		.pipe(uglify({
-			mangle: true,
+			mangle  : true,
 			compress: {
-				sequences: true, // join consecutive statemets with the “comma operator”
-				dead_code: true, // discard unreachable code
+				sequences   : true, // join consecutive statemets with the “comma operator”
+				dead_code   : true, // discard unreachable code
 				conditionals: true, // optimize if-s and conditional expressions
-				booleans: true, // optimize boolean expressions
-				unused: true, // drop unused variables/functions
-				if_return: true, // optimize if-s followed by return/continue
-				join_vars: true, // join var declarations
+				booleans    : true, // optimize boolean expressions
+				unused      : true, // drop unused variables/functions
+				if_return   : true, // optimize if-s followed by return/continue
+				join_vars   : true, // join var declarations
 				drop_console: true // drop console
 			}
 		}))
@@ -140,22 +150,36 @@ gulp.task('scripts', function ()
 /**
  * Default task
  */
-gulp.task('default',['sprites','styles','scripts',], function() {
+gulp.task('default', ['sprites', 'styles', 'scripts', 'styleguide'], function ()
+{
 	livereload.listen();
+
+	//Sprite
 	gulp.watch([
 		'../src/sprites/**/*.png',
-	],['sprites']);
+	], ['sprites']);
+
+	//Sass
 	gulp.watch([
 		'../src/*.scss',
 		'../src/**/*.scss',
 		'../src/**/**/*.scss',
 		'../src/libs/**/*.scss',
 		'../src/libs/**/**/*.scss',
-	],['styles']);
+	], ['styles']);
+
+	//Js
 	gulp.watch([
 		'../src/*.js',
 		'../src/**/*.js',
 		'../src/**/**/*.js',
 		'../src/libs/**/*.js',
-	],['scripts']);
+	], ['scripts']);
+
+	//Styleguide
+	gulp.watch([
+		'../dist/**/*.js',
+		'../dist/**/*.css',
+		'../styleguide/config.md'
+	], ['styleguide']);
 });
--- styleguide/config.md
+++ styleguide/config.md
@@ -2,11 +2,12 @@
 
 ### Head
 
-    link(rel="stylesheet" href="../dist/css/index.min.css")
+    link(rel="stylesheet" href="../wp-content/themes/dfwp_child/dist/css/index.min.css")
+    link(rel="stylesheet" href="../wp-content/themes/dfwp_child/dist/css/pattern.min.css")
     link(rel='stylesheet' href='https://cdn.rawgit.com/styledown/styledown/v1.0.2/data/styledown.css')
     script(src='https://cdn.rawgit.com/styledown/styledown/v1.0.2/data/styledown.js')
 
 ### Body
 
-    h1 My Awesome Styleguides
+    h1 Styleguide
     div#styleguides(sg-content)
